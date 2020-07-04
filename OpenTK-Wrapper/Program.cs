4b72951633ecf9f61e6e0df5d821554241d49d22
max <mathemaxis@gmail.com>
2020-07-04 19:31:18 +0100

    Added repository. FC.
    Added repository. FC.

---
+++ OpenTK-Wrapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenTKWrapper
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
